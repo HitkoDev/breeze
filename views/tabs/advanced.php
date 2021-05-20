7f797c15a70e0dee0990ad403ce4b6a04664bc23
adeelkhan <adeelkhan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2021-05-20 08:50:44 +0000

    advanced update 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/breeze/trunk@2534698 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/tabs/advanced.php
+++ views/tabs/advanced.php
@@ -45,6 +45,12 @@ if ( ! isset( $advanced['breeze-group-css'] ) ) {
 if ( ! isset( $advanced['breeze-group-js'] ) ) {
 	$advanced['breeze-group-js'] = '0';
 }
+
+if ( ! isset( $advanced['breeze-enable-js-delay'] ) ) {
+	$advanced['breeze-enable-js-delay'] = '0';
+}
+
+$js_inline_enable = filter_var( $advanced['breeze-enable-js-delay'], FILTER_VALIDATE_BOOLEAN );
 ?>
 <table cellspacing="15" id="advanced-options-tab">
     <tr>
@@ -100,11 +106,11 @@ if ( ! isset( $advanced['breeze-group-js'] ) ) {
             </span>
         </td>
     </tr>
-	<tr>
-		<td>
-			<label for="exclude-urls" class="breeze_tool_tip"><?php _e( 'Never Cache these URLs', 'breeze' ); ?></label>
-		</td>
-		<td>
+    <tr>
+        <td>
+            <label for="exclude-urls" class="breeze_tool_tip"><?php _e( 'Never Cache these URLs', 'breeze' ); ?></label>
+        </td>
+        <td>
 			<?php
 			$css_output = '';
 			if ( ! empty( $advanced['breeze-exclude-urls'] ) ) {
@@ -112,46 +118,46 @@ if ( ! isset( $advanced['breeze-group-js'] ) ) {
 				$css_output = esc_textarea( $output );
 			}
 			?>
-			<textarea cols="100" rows="7" id="exclude-urls" name="exclude-urls"><?php echo $css_output; ?></textarea>
-			<br/>
-			<span class="breeze_tool_tip"><b>Note:&nbsp;</b><?php _e( 'Add the URLs of the pages (one per line) you wish to exclude from the WordPress internal cache. To exclude URLs from the Varnish cache, please refer to this ', 'breeze' ); ?><a
-						href="https://support.cloudways.com/how-to-exclude-url-from-varnish/"
-						target="_blank"><?php _e( 'Knowledge Base', 'breeze' ); ?></a><?php _e( ' article.', 'breeze' ); ?> </span>
+            <textarea cols="100" rows="7" id="exclude-urls" name="exclude-urls"><?php echo $css_output; ?></textarea>
+            <br/>
+            <span class="breeze_tool_tip"><b>Note:&nbsp;</b><?php _e( 'Add the URLs of the pages (one per line) you wish to exclude from the WordPress internal cache. To exclude URLs from the Varnish cache, please refer to this ', 'breeze' ); ?><a
+                        href="https://support.cloudways.com/how-to-exclude-url-from-varnish/"
+                        target="_blank"><?php _e( 'Knowledge Base', 'breeze' ); ?></a><?php _e( ' article.', 'breeze' ); ?> </span>
 			<?php if ( false === $excluded_url_list ) { ?>
-				<br/>
-				<span class="breeze_tool_tip" style="color: #ff0000">
+                <br/>
+                <span class="breeze_tool_tip" style="color: #ff0000">
 					<?php _e( 'One (or more) URL is invalid. Please check and correct the entry.', 'breeze' ); ?>
 				</span>
 			<?php } ?>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<label class="breeze_tool_tip"><?php _e( 'Group Files', 'breeze' ); ?></label>
-		</td>
-		<td>
-			<ul>
-				<li>
-					<input type="checkbox" name="group-css" id="group-css"
-						   value="1" <?php checked( $advanced['breeze-group-css'], '1' ); ?>/>
-					<label class="breeze_tool_tip" for="group-css"><?php _e( 'CSS', 'breeze' ); ?></label>
-				</li>
-				<li>
-					<input type="checkbox" name="group-js" id="group-js"
-						   value="1" <?php checked( $advanced['breeze-group-js'], '1' ); ?>/>
-					<label class="breeze_tool_tip" for="group-js"><?php _e( 'JS', 'breeze' ); ?></label>
-				</li>
-				<li>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label class="breeze_tool_tip"><?php _e( 'Group Files', 'breeze' ); ?></label>
+        </td>
+        <td>
+            <ul>
+                <li>
+                    <input type="checkbox" name="group-css" id="group-css"
+                           value="1" <?php checked( $advanced['breeze-group-css'], '1' ); ?>/>
+                    <label class="breeze_tool_tip" for="group-css"><?php _e( 'CSS', 'breeze' ); ?></label>
+                </li>
+                <li>
+                    <input type="checkbox" name="group-js" id="group-js"
+                           value="1" <?php checked( $advanced['breeze-group-js'], '1' ); ?>/>
+                    <label class="breeze_tool_tip" for="group-js"><?php _e( 'JS', 'breeze' ); ?></label>
+                </li>
+                <li>
 					<span class="breeze_tool_tip">
 						<b>Note:&nbsp;</b><?php _e( 'Group CSS and JS files to combine them into a single file. This will reduce the number of HTTP requests to your server.', 'breeze' ); ?><br>
 						<b><?php _e( 'Important: Enable Minification to use this option.', 'breeze' ); ?></b>
 					</span>
-				</li>
-			</ul>
-		</td>
-	</tr>
-	<tr>
-		<td>
+                </li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td>
             <label class="breeze_tool_tip"><?php _e( 'Preload links', 'breeze' ); ?></label>
         </td>
         <td>
@@ -167,9 +173,9 @@ if ( ! isset( $advanced['breeze-group-js'] ) ) {
     </tr>
     <tr>
         <td>
-			<label for="exclude-css" class="breeze_tool_tip"><?php _e( 'Exclude CSS', 'breeze' ); ?></label>
-		</td>
-		<td>
+            <label for="exclude-css" class="breeze_tool_tip"><?php _e( 'Exclude CSS', 'breeze' ); ?></label>
+        </td>
+        <td>
 			<?php
 			$css_output = '';
 			if ( ! empty( $advanced['breeze-exclude-css'] ) ) {
@@ -177,23 +183,23 @@ if ( ! isset( $advanced['breeze-group-js'] ) ) {
 				$css_output = esc_textarea( $output );
 			}
 			?>
-			<textarea cols="100" rows="7" id="exclude-css" name="exclude-css"><?php echo $css_output; ?></textarea>
+            <textarea cols="100" rows="7" id="exclude-css" name="exclude-css"><?php echo $css_output; ?></textarea>
 			<?php if ( false === $excluded_css_check_extension ) { ?>
-				<br/><span class="breeze_tool_tip"
-						   style="color: #ff0000"><?php _e( 'One (or more) URL is incorrect. Please confirm that all URLs have the .css extension', 'breeze' ); ?></span>
+                <br/><span class="breeze_tool_tip"
+                           style="color: #ff0000"><?php _e( 'One (or more) URL is incorrect. Please confirm that all URLs have the .css extension', 'breeze' ); ?></span>
 			<?php } ?>
 			<?php if ( false === $excluded_css_check ) { ?>
-				<br/><span class="breeze_tool_tip" style="color: #ff0000"><?php _e( 'One (or more) URL is invalid. Please check and correct the entry.', 'breeze' ); ?></span>
+                <br/><span class="breeze_tool_tip" style="color: #ff0000"><?php _e( 'One (or more) URL is invalid. Please check and correct the entry.', 'breeze' ); ?></span>
 			<?php } ?>
-			<br/>
-			<span class="breeze_tool_tip"><b>Note:&nbsp;</b><?php _e( 'Use this option to exclude CSS files from Minification and Grouping. Enter the URLs of CSS files on each line.', 'breeze' ); ?></span>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<label for="exclude-js" class="breeze_tool_tip"><?php _e( 'Exclude JS', 'breeze' ); ?></label>
-		</td>
-		<td>
+            <br/>
+            <span class="breeze_tool_tip"><b>Note:&nbsp;</b><?php _e( 'Use this option to exclude CSS files from Minification and Grouping. Enter the URLs of CSS files on each line.', 'breeze' ); ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label for="exclude-js" class="breeze_tool_tip"><?php _e( 'Exclude JS', 'breeze' ); ?></label>
+        </td>
+        <td>
 			<?php
 			$js_output = '';
 			if ( ! empty( $advanced['breeze-exclude-js'] ) ) {
@@ -201,20 +207,20 @@ if ( ! isset( $advanced['breeze-group-js'] ) ) {
 				$js_output = esc_textarea( $output );
 			}
 			?>
-			<textarea cols="100" rows="7" id="exclude-js" name="exclude-js"><?php echo $js_output; ?></textarea>
+            <textarea cols="100" rows="7" id="exclude-js" name="exclude-js"><?php echo $js_output; ?></textarea>
 			<?php if ( false === $excluded_js_check_extension ) { ?>
-				<br/><span class="breeze_tool_tip"
-						   style="color: #ff0000"><?php _e( 'One (or more) URL is incorrect. Please confirm that all URLs have the .js extension', 'breeze' ); ?></span>
+                <br/><span class="breeze_tool_tip"
+                           style="color: #ff0000"><?php _e( 'One (or more) URL is incorrect. Please confirm that all URLs have the .js extension', 'breeze' ); ?></span>
 			<?php } ?>
 			<?php if ( false === $excluded_js_check ) { ?>
-				<br/><span class="breeze_tool_tip" style="color: #ff0000"><?php _e( 'One (or more) URL is invalid. Please check and correct the entry.', 'breeze' ); ?></span>
+                <br/><span class="breeze_tool_tip" style="color: #ff0000"><?php _e( 'One (or more) URL is invalid. Please check and correct the entry.', 'breeze' ); ?></span>
 			<?php } ?>
-			<br/>
-			<span class="breeze_tool_tip"><b>Note:&nbsp;</b><?php _e( 'Use this option to exclude JS files from Minification and Grouping. Enter the URLs of JS files on each line.', 'breeze' ); ?></span>
-		</td>
-	</tr>
-	<tr>
-		<td>
+            <br/>
+            <span class="breeze_tool_tip"><b>Note:&nbsp;</b><?php _e( 'Use this option to exclude JS files from Minification and Grouping. Enter the URLs of JS files on each line.', 'breeze' ); ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
             <label for="breeze-preload-font" class="breeze_tool_tip"><?php _e( 'Preload your webfonts', 'breeze' ); ?></label>
         </td>
         <td>
@@ -271,130 +277,149 @@ if ( ! isset( $advanced['breeze-group-js'] ) ) {
 				</span>
                 <br/>
                 <span class="breeze_tool_tip">
-                    <?php $theme_url = get_template_directory_uri().'/assets/fonts/my-font.woff';?>
-					<?php _e( 'Example:<code>'.$theme_url.'</code>', 'breeze' ); ?>
+                    <?php $theme_url = get_template_directory_uri() . '/assets/fonts/my-font.woff'; ?>
+                    <?php _e( 'Example:<code>' . $theme_url . '</code>', 'breeze' ); ?>
 				</span>
             </div>
         </td>
     </tr>
-	<tr>
-		<td>
-			<label for="move-to-footer-js" class="breeze_tool_tip"><?php _e( 'Move JS files to footer', 'breeze' ); ?></label>
-		</td>
-		<td>
-			<div class="breeze-list-url">
+    <tr>
+        <td>
+            <label for="move-to-footer-js" class="breeze_tool_tip"><?php _e( 'Move JS files to footer', 'breeze' ); ?></label>
+        </td>
+        <td>
+            <div class="breeze-list-url">
 				<?php if ( ! empty( $advanced['breeze-move-to-footer-js'] ) ) : ?>
 					<?php foreach ( $advanced['breeze-move-to-footer-js'] as $js_url ) : ?>
-						<div class="breeze-input-group">
+                        <div class="breeze-input-group">
 					<span class="sort-handle">
 						<span class="dashicons dashicons-arrow-up moveUp"></span>
 						<span class="dashicons dashicons-arrow-down moveDown"></span>
 					</span>
-							<input type="text" size="98"
-								   class="breeze-input-url"
-								   name="move-to-footer-js[]"
-								   placeholder="<?php _e( 'Enter URL...', 'breeze' ); ?>"
-								   value="<?php echo esc_html( $js_url ); ?>"/>
-							<span class="dashicons dashicons-no item-remove" title="<?php _e( 'Remove', 'breeze' ); ?>"></span>
-						</div>
+                            <input type="text" size="98"
+                                   class="breeze-input-url"
+                                   name="move-to-footer-js[]"
+                                   placeholder="<?php _e( 'Enter URL...', 'breeze' ); ?>"
+                                   value="<?php echo esc_html( $js_url ); ?>"/>
+                            <span class="dashicons dashicons-no item-remove" title="<?php _e( 'Remove', 'breeze' ); ?>"></span>
+                        </div>
 					<?php endforeach; ?>
 				<?php else : ?>
-					<div class="breeze-input-group">
+                    <div class="breeze-input-group">
 					<span class="sort-handle">
 						<span class="dashicons dashicons-arrow-up moveUp"></span>
 						<span class="dashicons dashicons-arrow-down moveDown"></span>
 					</span>
-						<input type="text" size="98"
-							   class="breeze-input-url"
-							   id="move-to-footer-js"
-							   name="move-to-footer-js[]"
-							   placeholder="<?php _e( 'Enter URL...', 'breeze' ); ?>"
-							   value=""/>
-						<span class="dashicons dashicons-no" title="<?php _e( 'Remove', 'breeze' ); ?>"></span>
-					</div>
+                        <input type="text" size="98"
+                               class="breeze-input-url"
+                               id="move-to-footer-js"
+                               name="move-to-footer-js[]"
+                               placeholder="<?php _e( 'Enter URL...', 'breeze' ); ?>"
+                               value=""/>
+                        <span class="dashicons dashicons-no" title="<?php _e( 'Remove', 'breeze' ); ?>"></span>
+                    </div>
 				<?php endif; ?>
-			</div>
-			<div style="margin: 10px 0">
-				<button type="button" class="button add-url" id="add-move-to-footer-js">
+            </div>
+            <div style="margin: 10px 0">
+                <button type="button" class="button add-url" id="add-move-to-footer-js">
 					<?php _e( 'Add URL', 'breeze' ); ?>
-				</button>
-			</div>
-			<div>
+                </button>
+            </div>
+            <div>
 				<span class="breeze_tool_tip">
 					<b>Note:&nbsp;</b>
 					<?php _e( 'Enter the complete URLs of JS files to be moved to the footer during minification process.', 'breeze' ); ?>
 				</span>
-				<span class="breeze_tool_tip">
+                <span class="breeze_tool_tip">
 					<?php _e( 'You should add the URL of original files as URL of minified files are not supported.', 'breeze' ); ?>
 				</span>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<label for="defer-js" class="breeze_tool_tip"><?php _e( 'JS files with deferred loading', 'breeze' ); ?></label>
-		</td>
-		<td>
-			<div class="breeze-list-url">
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label for="defer-js" class="breeze_tool_tip"><?php _e( 'JS files with deferred loading', 'breeze' ); ?></label>
+        </td>
+        <td>
+            <div class="breeze-list-url">
 				<?php if ( ! empty( $advanced['breeze-defer-js'] ) ) : ?>
 					<?php foreach ( $advanced['breeze-defer-js'] as $js_url ) : ?>
-						<div class="breeze-input-group">
+                        <div class="breeze-input-group">
 							<span class="sort-handle">
 								<span class="dashicons dashicons-arrow-up moveUp"></span>
 								<span class="dashicons dashicons-arrow-down moveDown"></span>
 							</span>
-							<input type="text" size="98"
-								   class="breeze-input-url"
-								   name="defer-js[]"
-								   placeholder="<?php _e( 'Enter URL...', 'breeze' ); ?>"
-								   value="<?php echo esc_html( $js_url ); ?>"/>
-							<span class="dashicons dashicons-no item-remove" title="<?php _e( 'Remove', 'breeze' ); ?>"></span>
-						</div>
+                            <input type="text" size="98"
+                                   class="breeze-input-url"
+                                   name="defer-js[]"
+                                   placeholder="<?php _e( 'Enter URL...', 'breeze' ); ?>"
+                                   value="<?php echo esc_html( $js_url ); ?>"/>
+                            <span class="dashicons dashicons-no item-remove" title="<?php _e( 'Remove', 'breeze' ); ?>"></span>
+                        </div>
 					<?php endforeach; ?>
 				<?php else : ?>
-					<div class="breeze-input-group">
+                    <div class="breeze-input-group">
 						<span class="sort-handle">
 							<span class="dashicons dashicons-arrow-up moveUp"></span>
 							<span class="dashicons dashicons-arrow-down moveDown"></span>
 						</span>
-						<input type="text" size="98"
-							   class="breeze-input-url"
-							   name="defer-js[]"
-							   id="defer-js"
-							   placeholder="<?php _e( 'Enter URL...', 'breeze' ); ?>"
-							   value=""/>
-						<span class="dashicons dashicons-no item-remove" title="<?php _e( 'Remove', 'breeze' ); ?>"></span>
-					</div>
+                        <input type="text" size="98"
+                               class="breeze-input-url"
+                               name="defer-js[]"
+                               id="defer-js"
+                               placeholder="<?php _e( 'Enter URL...', 'breeze' ); ?>"
+                               value=""/>
+                        <span class="dashicons dashicons-no item-remove" title="<?php _e( 'Remove', 'breeze' ); ?>"></span>
+                    </div>
 				<?php endif; ?>
-			</div>
-			<div style="margin: 10px 0">
-				<button type="button" class="button add-url" id="add-defer-js">
+            </div>
+            <div style="margin: 10px 0">
+                <button type="button" class="button add-url" id="add-defer-js">
 					<?php _e( 'Add URL', 'breeze' ); ?>
-				</button>
-			</div>
-			<div>
+                </button>
+            </div>
+            <div>
 				<span class="breeze_tool_tip">
 					<b>Note:&nbsp;</b>
 					<?php _e( 'You should add the URL of original files as URL of minified files are not supported.', 'breeze' ); ?>
 				</span>
-			</div>
-		</td>
-	</tr>
+            </div>
+        </td>
+    </tr>
     <tr>
         <td>
             <label for="delay-js-scripts" class="breeze_tool_tip"><?php _e( 'Delay JS inline scripts', 'breeze' ); ?></label>
         </td>
         <td>
+            <input type="checkbox" name="enable-js-delay" id="enable-js-delay"
+                   value="1" <?php checked( $advanced['breeze-enable-js-delay'], '1' ); ?>/>
+            <label class="breeze_tool_tip" for="enable-js-delay"><?php _e( 'Enable delay inline JavaScript', 'breeze' ); ?></label>
+            <br/>
+            <br/>
 			<?php
 			$js_output = '';
 			if ( ! empty( $advanced['breeze-delay-js-scripts'] ) ) {
 				$output    = implode( "\n", $advanced['breeze-delay-js-scripts'] );
 				$js_output = esc_textarea( $output );
 			}
+
+			$display_text_area = 'style="display:none"';
+			if ( true === $js_inline_enable ) {
+				$display_text_area = 'style="display:block"';
+			}
 			?>
-            <textarea cols="100" rows="7" id="delay-js-scripts" name="delay-js-scripts"><?php echo $js_output; ?></textarea>
-            <br/>
-            <span class="breeze_tool_tip"><b>Note:&nbsp;</b><?php _e( 'You can add specififc keywords to identify the inline JavaScript to be delayed. Each script identifying keyword must be added on a new line.', 'breeze' ); ?></span>
+            <div <?php echo $display_text_area; ?> id="breeze-delay-js-scripts-div">
+                <textarea cols="100" rows="7" id="delay-js-scripts" name="delay-js-scripts"><?php echo $js_output; ?></textarea>
+                <br/>
+                <span class="breeze_tool_tip">
+                    <strong>Notes:&nbsp;</strong> <br/>
+                    <?php _e( 'You can add specififc keywords to identify the inline JavaScript to be delayed. Each script identifying keyword must be added on a new line.', 'breeze' ); ?>
+                    <a href="https://www.cloudways.com/blog/breeze-1-2-version-released/" target="_blank"><?php _e( 'More info here', 'breeze' ); ?></a><br/>
+                    <span style="color: #ff0000">
+                         <?php _e( 'Please clear Varnish after applying the new settings.', 'breeze' ); ?>
+                    </span>
+                </span>
+            </div>
         </td>
     </tr>
 </table>
