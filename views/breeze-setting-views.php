b3748053369775402dc13d4bb7a0a8849d15f53d
adeelkhan <adeelkhan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2021-07-12 10:55:44 +0000

    update for 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/breeze/trunk@2562740 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/breeze-setting-views.php
+++ views/breeze-setting-views.php
@@ -8,7 +8,7 @@ $tabs = array(
 	'database' => __( 'DATABASE', 'breeze' ),
 	'cdn'      => __( 'CDN', 'breeze' ),
 	'varnish'  => __( 'VARNISH', 'breeze' ),
-	'import_export' => __( 'Settings import/export', 'breeze' ),
+	'import_export' => __( 'IMPORT/EXPORT', 'breeze' ),
 	'faq'      => __( 'FAQs', 'breeze' ),
 );
 
