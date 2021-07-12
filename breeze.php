d523d2a4b1046dcdaebfdc9df7576b876414912e
adeelkhan <adeelkhan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2021-07-12 10:56:16 +0000

    update for 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/breeze/trunk@2562742 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- breeze.php
+++ breeze.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Breeze
  * Description: Breeze is a WordPress cache plugin with extensive options to speed up your website. All the options including Varnish Cache are compatible with Cloudways hosting.
- * Version: 1.2.2
+ * Version: 1.2.3
  * Text Domain: breeze
  * Domain Path: /languages
  * Author: Cloudways
@@ -37,7 +37,7 @@ if ( ! defined( 'BREEZE_PLUGIN_DIR' ) ) {
 	define( 'BREEZE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 }
 if ( ! defined( 'BREEZE_VERSION' ) ) {
-	define( 'BREEZE_VERSION', '1.2.2' );
+	define( 'BREEZE_VERSION', '1.2.3' );
 }
 if ( ! defined( 'BREEZE_SITEURL' ) ) {
 	define( 'BREEZE_SITEURL', get_site_url() );
